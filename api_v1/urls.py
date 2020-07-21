8a26988ba2d7989c37cb2ff82061c03e50a586bd
JhonSanz <ingjhonsanz@utp.edu.co>
2020-07-21 18:24:49 -0500

    login

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views.visitor import VisitorApi
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 app_name = "api_v1"
 urlpatterns = [
-    path('visitor', VisitorApi.as_view()),
+    path('visitor', VisitorApi.as_view(), name="visitor_general"),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- quarantine/urls.py
+++ quarantine/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("v1", include("api_v1.urls"))
+    path("api/v1/", include("api_v1.urls")),
 ]
