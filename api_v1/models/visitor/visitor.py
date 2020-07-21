6b7d9545f0fab76404388018e39f8b1b57fad320
JhonSanz <ingjhonsanz@utp.edu.co>
2020-07-21 18:12:46 -0500

    visitor model

---
+++ api_v1/models/visitor/visitor.py
@@ -0,0 +1,23 @@
+""" Contains the visitor model """
+
+from django.db import models
+
+
+class Visitor(models.Model):
+    """ Category model definition """
+
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    temperature = models.PositiveIntegerField(default=36.1)
+    date_visited = models.DateTimeField(auto_now=True)
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """ Sets human readable name """
+        verbose_name = "Visitante"
+
+    def get_visitor_info(self):
+        """ Returns basic visitor info """
+        return "{} {}°C".format(self.name, self.temperature)
+
+    def __str__(self):
+        return self.name
--- api_v1/views/visitor/general.py
+++ api_v1/views/visitor/general.py
@@ -1,4 +1,4 @@
-""" """
+""" General visitor api endpoints """
 
 from cerberus import Validator
 from rest_framework import status
@@ -10,18 +10,19 @@ from ...serializers.visitor import VisitorSerializer
 
 
 class VisitorApi(APIView):
-    """ """
+    """ GET and POST http verbs """
 
     def get(self, request):
         """
         """
+
         validator = Validator({
-            "name": {"required": False, "empty": False, "type": "string"}
+            "name": {"required": False, "empty": False, "type": "string"},
             "email": {"required": False, "empty": False, "type": "string",
-                      "regex": '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'}
-            "temperature": {"required": False, "empty": False, "type": "number"}
+                      "regex": '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'},
+            "temperature": {"required": False, "empty": False, "type": "number"},
             "date_visited": {
-                "required": False, "empty": False, "type": "date"}
+                "required": False, "empty": False, "type": "datetime"}
         })
         if not validator.validate(request.GET):
             return Response({
@@ -44,3 +45,23 @@ class VisitorApi(APIView):
             VisitorSerializer(Visitor.objects.filter(
                 *filters), many=True).data,
             status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        """
+
+        validator = Validator({
+            "name": {"required": False, "empty": False, "type": "string"},
+            "email": {"required": False, "empty": False, "type": "string",
+                      "regex": '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'},
+            "temperature": {"required": False, "empty": False, "type": "number"}
+        })
+        if not validator.validate(request.GET):
+            return Response({
+                "code": "invalid_body",
+                "detail": "Invalid request data",
+                "data": validator.errors
+            })
+        
+        Visitor.objects.create(**request.data)
+        return Response(status=status.HTTP_200_OK)
